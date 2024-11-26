95c9bf410b631c84cd03c014004e16c0e1e9cc47
Jatin <jatinsainii007@gmail.com>
2024-11-26 23:22:49 +0530

    Users name

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,23 @@ import {getDatabase, set,ref} from "firebase/database"
 import { app } from './firebase'
 
 import './App.css'
-
-//this is how database is created and data is sent in RealTimeDatabase
-// const db  = getDatabase(app)
-
-// function App() {
-  
-//   const putdata = ()=>{
-//     set(ref(db, 'users/jatin'),{
-//       id : 1,
-//       name : "Jatin",
-//       age : 21
-//     })
-//   }
-
-
-import {createUserWithEmailAndPassword, getAuth , onAuthStateChanged } from 'firebase/auth'
+import { getAuth} from 'firebase/auth'
 import SignUpPage from './pages/SignUp'
 import SignInPage from './pages/SignIn'
-import { useEffect } from 'react'
+import ListUsers from './pages/ListUsers'
 
-const auth = getAuth(app);   // this is how we create an instance(auth) for different operations 
+
+const auth = getAuth(app);   // this is how we create an instance(auth) for different operations
+
 
 
 function App(){
 
-
  return (
   <div>
-    <SignUpPage/>
-    <SignInPage/>
+    {/* <SignUpPage/>
+    <SignInPage/> */}
+    <ListUsers/>
   </div>
  )
 }
